5aba537c4efd6e34bea224fc2894fe83f06181d3
PSHEMA <p.shema@alustudent.com>
2023-09-11 11:45:37 -0700

    minimum operations

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Minimum Operations
+"""
+
+def minOperations(n):
+    if n <= 1:
+        return 0
+
+    operations = 0
+    i = 2
+
+    while i <= n:
+        if n % i == 0:
+            n //= i
+            operations += i
+        else:
+            i += 1
+
+    return operations
