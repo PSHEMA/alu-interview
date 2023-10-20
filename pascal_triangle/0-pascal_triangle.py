69ce98efe1e2699a03d47be6b44fee1b24f9809b
PSHEMA <p.shema@alustudent.com>
2023-10-20 19:57:42 +0200

    PT

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -3,14 +3,14 @@
 
 
 def pascal_triangle(n):
-    """ Pascal's Triangle """
+    """Pascal's Triangle"""
     if n <= 0:
         return []
     triangle = [[1]]
     for i in range(n):
-        new_row = [1]
-        for j in range(len(triangle[i]) - 1):
-            new_row.append(triangle[i][j] + triangle[i][j + 1])
-        new_row.append(1)
-        triangle.append(new_row)
+        row = [1]
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        triangle.append(row)
     return triangle
